b94ba443830bc166d816b13899fabbed3785b7f9
dump501 <tsafack07@gmail.com>
2023-10-21 11:15:16 +0100

    add bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PlaylistComponent } from './playlist/playlist.component';
+import { ChannelComponent } from './channel/channel.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "playlist", component: PlaylistComponent},
+  {path: "channel", component: ChannelComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
